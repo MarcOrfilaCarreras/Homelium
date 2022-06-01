61dc9b7d68d4034331088574ee4293f8c39f9f6a
MarcOrfilaCarreras <marcorfila2003@gmail.com>
2022-06-01 16:09:50 +0200

    Added: Dropdown list that shows existing groups and images

--- app/modules/add.py
+++ app/modules/add.py
@@ -1,24 +1,34 @@
 import json
+from operator import contains
 from flask import flash, redirect, render_template, request
 import requests
 
 def mainpage():
-    return render_template("new.html")
+    with open("app/config.json", 'r') as f:
+        links_dict = json.load(f)
+
+    return render_template("new.html", links_dict = links_dict)
 
 def addApp():
 
     with open("app/config.json", 'r') as f:
         links_dict = json.load(f)
 
-    try:
-        response = requests.get(request.form["image"])
+    image = ""
+    if ( "http" or "https") in request.form["image"]:
+        try:
+            response = requests.get(request.form["image"])
+            file = open("app/static/img/" + request.form["name"] + ".png", "wb")
+            file.write(response.content)
+            file.close()
 
-        file = open("app/static/img/" + request.form["name"] + ".png", "wb")
-        file.write(response.content)
-        file.close()
-    
-    except:
-        flash("The image couldn't be downloaded !")
+            image = "static/img/" + request.form["name"] + ".png"
+        
+        except:
+            image = "static/img/default.png"
+            flash("The image couldn't be downloaded !")
+    else:
+        image = request.form["image"]
 
     counter = 0
     found = False
@@ -27,7 +37,7 @@ def addApp():
 
             found = True
 
-            links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/img/" + request.form["name"] + ".png", "url":request.form["url"]})
+            links_dict[counter]["apps"].append({"name":request.form["name"], "image":image, "url":request.form["url"]})
 
             with open("app/config.json", 'w', encoding='utf-8') as f:
                 json.dump(links_dict, f, ensure_ascii=False, indent=4)
@@ -36,7 +46,7 @@ def addApp():
     
     if found == False:
         links_dict.append({"name":request.form["group"], "apps":[]})
-        links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/img/" + request.form["name"] + ".png", "url":request.form["url"]})
+        links_dict[counter]["apps"].append({"name":request.form["name"], "image":image, "url":request.form["url"]})
         
         with open("app/config.json", 'w', encoding='utf-8') as f:
             json.dump(links_dict, f, ensure_ascii=False, indent=4)
